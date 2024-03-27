5d16add641a209c08dc684ba91882f2e215b18ac
bennie <>
2024-03-27 12:19:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ /*
+   main template file.
+   *
+   * 
+   * @package Aquila
+  */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aquila</title>
+</head>
+<body>
+  <h1>Advance Theme Development</h1>
+</body>
+</html>
